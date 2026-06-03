5bd6252e2c520fc992d9be48a0f59ad77e4288c1
VAIBHAV <imvksofficial@gmail.com>
2026-06-03 20:36:43 +0530

    completed all the logics for controllers and routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,18 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+
+
 const app=express()
 
 app.use(cors({
@@ -15,9 +27,6 @@ app.use(express.static("public"))
 
 app.use(cookieParser())
 
-//routes import
-import userRouter from "./routes/user.routes.js"
-import subscriptionRouter from "./routes/subscription.routes.js"
 
 
 
@@ -26,6 +35,12 @@ import subscriptionRouter from "./routes/subscription.routes.js"
 //industry level practice we will use middlewares ie... app.use()
 app.use("/api/v1/users",userRouter)
 app.use("/api/v1/subscriptions",subscriptionRouter)
+app.use("/api/v1/videos",videoRouter)
+app.use("/api/v1/comments",commentRouter)
+app.use("/api/v1/healthcheck",healthcheckRouter)
+app.use("/api/v1/tweets",tweetRouter)
+app.use("/api/v1/likes",likeRouter)
+app.use("/api/v1/playlists",playlistRouter)
 
 
 //https://localhost:8000/api/v1/users/register
