21696a179ee25b3db45ae2ee576637d43b4fc982
VAIBHAV <imvksofficial@gmail.com>
2026-06-07 13:16:52 +0530

    bug fixed

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import mongoose from "mongoose";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.model.js";
 import { Video } from "../models/video.model.js";
 import uploadOnCloudinary from "../utils/cloudinary.js";
 
