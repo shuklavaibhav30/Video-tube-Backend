c698bb677262ce24c9d837b26c56a3ff22af84c0
VAIBHAV <imvksofficial@gmail.com>
2026-06-10 17:23:29 +0530

    fix: update comment routes to include optional user authentication for getting video comments

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -8,12 +8,13 @@ import {
 } from "../controllers/comment.controller.js"
 
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { getOptionalUser } from "../middlewares/optionalAuth.middleware.js";
 
 const router=Router();
-router.use(verifyJWT);//        Apply verifyJWT middleware to all routes in this file
+//router.use(verifyJWT);//        Apply verifyJWT middleware to all routes in this file
 
 
-router.route("/:videoId").get(getVideoComments).post(addComment)
-router.route("/c/:commentId").delete(deleteComment).patch(updateComment);
+router.route("/:videoId").get(getOptionalUser,getVideoComments).post(verifyJWT,addComment)
+router.route("/c/:commentId").delete(verifyJWT,deleteComment).patch(verifyJWT,updateComment);
 
 export default router
